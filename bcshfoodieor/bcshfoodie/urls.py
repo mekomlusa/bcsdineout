4c59c881837bd2f64c581e70556255c985008209
mekomlusa <meko.mlusa@gmail.com>
2017-09-23 20:32:37 -0500

    Ready for production 2

---
+++ bcshfoodieor/bcshfoodie/urls.py
@@ -0,0 +1,39 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
+
+# Use include() to add URLS from the catalog application 
+from django.conf.urls import include
+
+urlpatterns += [
+    url(r'^catalog/', include('catalog.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
+from django.conf.urls import url
+
+from catalog import views
+
+
+urlpatterns += [
+        url(r'^$', views.index, name='index'),
+
+]
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^signup/$', views.signup, name='user-signup'),
+    url(r'^search-form/$', views.search_form, name='search'),
+    url(r'^search/$', views.search2, name='result'),
+    
+]
