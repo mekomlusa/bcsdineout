8d12e31b2d709728f29b1988f8d92d5b0c887982
mekomlusa <meko.mlusa@gmail.com>
2017-09-25 18:25:18 -0500

    signup form update 2

--- catalog/forms.py
+++ catalog/forms.py
@@ -10,13 +10,10 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
-        help_texts = {
-            'password1': 'Your password can''t be too similar to your other personal information. Your password must contain at least 8 characters. Your password can''t be a commonly used password. Your password can''t be entirely numeric.',
-        }
+    
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].help_text = 'Your password can''t be too similar to your other personal information. Your password must contain at least 8 characters. Your password can''t be a commonly used password. Your password can''t be entirely numeric.'
         
 class NoteForm(forms.ModelForm):
     note = forms.CharField(max_length=1000,
