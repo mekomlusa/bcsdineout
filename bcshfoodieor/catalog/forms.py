95cb3ef07347f07cc95dd2cccaac671f948b17b9
mekomlusa <meko.mlusa@gmail.com>
2017-09-30 19:41:04 -0500

    Boostrap in production

--- bcshfoodieor/catalog/forms.py
+++ bcshfoodieor/catalog/forms.py
@@ -11,9 +11,9 @@ class SignUpForm(UserCreationForm):
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].help_text = 'Your password can''t be too similar to your other personal information. Your password must contain at least 8 characters. Your password can''t be a commonly used password. Your password can''t be entirely numeric.'
         
 class NoteForm(forms.ModelForm):
     note = forms.CharField(max_length=1000,
